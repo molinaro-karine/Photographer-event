7902c26a9e2a51370c9b3ed57c41bdf2b4caadbd
molinaro karine <molinaro.karine@hotmail.com>
2023-10-31 03:11:11 +0100

    hero:image aléatoire

--- photoevent/template-parts/content-hero.php
+++ photoevent/template-parts/content-hero.php
@@ -1,4 +1,17 @@
+
 <section class="hero">
-<h1 class="title"><?php the_title() ?></h1>
-  <img class="img-hero" src="<?php echo get_template_directory_uri(); ?>'/assets/images/nathalie-0.webp'" alt="hero">
+  <h1 class="title"><?php the_title() ?></h1>
+  <?php query_posts(
+      array(
+          'post_type' => 'photo',
+          'showposts' => 1,
+          'orderby' => 'rand',
+          'terms' => 'paysage',
+      )
+  ); ?>
+  <?php if (have_posts()) :
+      while (have_posts()) :
+        the_post(); ?>
+          <img class="img-hero" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title_attribute(); ?>"> 
+  <?php endwhile;endif; ?>
 </section>
